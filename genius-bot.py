c5678eac81de4429fc581d5d8d7312b2df447bb8
Tarik Desire <tprdesire@gmail.com>
2018-10-24 13:43:28 -0400

    Added !savelyricsfor keyword (saves specified song lyrics to bot`s host computer)

--- genius-bot.py
+++ genius-bot.py
@@ -9,7 +9,6 @@ TOKEN = config.TOKEN
 Client = discord.Client()
 client = commands.Bot(command_prefix = '!genius')
 
-
 @client.event
 async def on_ready():
   print('Bot is ready!')
@@ -17,17 +16,32 @@ async def on_ready():
 @client.event
 async def on_message(message):
   if message.content.upper().startswith('!LYRICSFOR'):
-    userID = message.author.id
     args = message.content.lower().replace("!lyricsfor", "").split("-")
     a = args[0]
     s = args[1]
-    print(a)
-    print(s)
     song = api.search_song(s, a).lyrics.split("\n")
     print(song)
     for line in song:
       if line == '':
         song.remove(line)
+      else:
+        await client.send_message(message.channel, "%s" % (line))
+  
+  if message.content.upper().startswith('!SAVELYRICSFOR'):
+    args = message.content.lower().replace("!savelyricsfor", "").split("-")
+    a = args[0]
+    s = args[1]
+    await client.send_message(message.channel, "Downloading the lyrics for  '{}'  by  '{}'  to the bot's host computer...".format(s, a))
+    song_to_save = api.search_song(s, a)
+    song_to_save.save_lyrics(format_='txt')
+
+    await client.send_message(message.channel, "Here is a snippet of the lyrics you've requested: ")
+    song_to_save_list = api.search_song(s, a).lyrics.split("\n")
+    snippet = song_to_save_list[1:8]
+
+    for line in snippet:
+      if line == '':
+        snippet.remove(line)
       else:
         await client.send_message(message.channel, "%s" % (line))
 
